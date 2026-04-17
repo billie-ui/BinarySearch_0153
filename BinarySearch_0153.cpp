071d93d38a763a41a755fe4b34bae23d5543fb0e
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-04-17 15:33:55 +0700

    Menambahkan fungsi bubble sort untuk mengurutkan array

--- BinarySearch_0153.cpp
+++ BinarySearch_0153.cpp
@@ -31,3 +31,21 @@ cout << "\n=======================================\n";
         cin >> element[i];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
